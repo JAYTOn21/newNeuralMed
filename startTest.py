b24179e3176fecebab6fc0b29bf2a9032cdfcdc4
JAYTOn <aleksakin1982@gmail.com>
2024-02-20 03:39:33 +0700

    multi train

--- startTest.py
+++ startTest.py
@@ -1,7 +1,7 @@
 import main
 import Settings
 
-for i in range(2):
+for i in range(Settings.testCount):
     net = main.Network(Settings.NS)
     X, Y, resX = main.reading()
     net.Train(X, Y, Settings.alpha, Settings.eps, Settings.epochs)
